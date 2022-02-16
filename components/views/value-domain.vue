9e1c12a5f38b15d3fd0b73b7382eb7282d5133c6
AMMAR <ammarbr@hotmail.com>
2022-02-16 09:23:16 +0100

    #38 - Add permittedValues table
    - Add permittedValues table to enumerated valueDomain
    - Add permittedValue detail view

--- components/views/value-domain.vue
+++ components/views/value-domain.vue
@@ -21,12 +21,46 @@
         <v-list-item-content>{{ $t('global.time') }}: {{ valueDomain.datetime.time }}</v-list-item-content>
         <v-list-item-content>{{ $t('global.hourFormat') }}: {{ valueDomain.datetime.hourFormat }}</v-list-item-content>
       </template>
+      <template v-if="valueDomain.type === 'ENUMERATED'">
+        <v-card>
+          <v-card-title>
+            PERMITTEDVALUES
+          </v-card-title>
+          <v-data-table
+            :headers="permittedValuesTable.headers"
+            :items="valueDomain.permittedValues"
+            :items-per-page="5"
+            class="elevation-1"
+            @click:row="handlePermittedValueClicked"
+          />
+        </v-card>
+      </template>
     </v-list-item-group>
+    <v-dialog
+      v-model="showPermittedValueDetailView"
+      width="600"
+    >
+      <v-card>
+        <PermittedValueDetailView
+          :urn="selectedPermittedValueUrn"
+          :editable="true"
+          :deletable="true"
+          @save="updateTree($event); snackbar.saveSuccess = true"
+          @saveFailure="snackbar.saveFailure = true"
+          @delete="updateTree(selectedElement) ; snackbar.deleteSuccess = true"
+          @deleteFailure="snackbar.deleteFailure = true"
+        />
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
 import Ajax from '~/config/ajax'
+import PermittedValueDetailView from '~/components/views/permitted-value-detail-view'
 export default {
+  components: {
+    PermittedValueDetailView
+  },
   props: {
     urn: {
       required: true,
@@ -38,7 +72,34 @@ export default {
       ajax: {
         dataElementUrl: process.env.mdrBackendUrl + '/v1/element/'
       },
-      valueDomain: undefined
+      valueDomain: undefined,
+      showPermittedValueDetailView: false,
+      selectedPermittedValueUrn: null,
+      permittedValuesTable: {
+        headers: [
+          {
+            text: this.$i18n.t('global.definition'),
+            align: 'start',
+            sortable: false,
+            value: 'definitions[0].definition'
+          },
+          {
+            text: this.$i18n.t('global.designation'),
+            sortable: false,
+            value: 'definitions[0].designation'
+          },
+          {
+            text: this.$i18n.t('global.language'),
+            sortable: false,
+            value: 'definitions[0].language'
+          },
+          {
+            text: this.$i18n.t('global.urn'),
+            sortable: false,
+            value: 'identification.urn'
+          }
+        ]
+      }
     }
   },
   watch: {
@@ -52,13 +113,17 @@ export default {
   methods: {
     async fetchValueDomain () {
       this.$log.debug('Fetching Value Domain of URN: ' + this.urn)
-      await this.$axios.$get(this.ajax.dataElementUrl + this.urn + '/valuedomain', Ajax.header.ignoreLanguage)
+      await this.$axios.$get(this.ajax.dataElementUrl + this.urn + '/valuedomain', Ajax.header.preferredLanguage)
         .then(function (res) {
           this.valueDomain = res
         }.bind(this))
         .catch(function (err) {
           this.$log.error('Unable to fetch ValueDomain:' + err)
         }.bind(this))
+    },
+    handlePermittedValueClicked (value) {
+      this.showPermittedValueDetailView = true
+      this.selectedPermittedValueUrn = value.identification.urn
     }
   }
 }
